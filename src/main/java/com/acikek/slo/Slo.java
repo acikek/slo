2bb9c5af5be43e8af96a6b58ad0b67d4f6b0f36b
Skye Prince <spiralixp@gmail.com>
2025-05-15 13:50:44 -0700

    improve connecting
    what a mess!

--- src/main/java/com/acikek/slo/Slo.java
+++ src/main/java/com/acikek/slo/Slo.java
@@ -1,11 +1,26 @@
 package com.acikek.slo;
 
+import com.acikek.slo.util.ServerLevelSummary;
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.ConnectScreen;
+import net.minecraft.client.gui.screens.DisconnectedScreen;
+import net.minecraft.client.gui.screens.GenericMessageScreen;
+import net.minecraft.client.multiplayer.ServerData;
+import net.minecraft.client.multiplayer.resolver.ServerAddress;
+import net.minecraft.network.chat.CommonComponents;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.progress.LoggerChunkProgressListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.*;
+import java.util.regex.Pattern;
 
 public class Slo implements ModInitializer {
 
@@ -15,6 +30,8 @@ public class Slo implements ModInitializer {
 
 	public static final String JAVA_PATH = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
 
+	public static final Pattern PREPARING_SPAWN = Pattern.compile("Preparing spawn area: ([0-9])+%");
+
 	public static Process serverProcess;
 
 	@Override
@@ -23,5 +40,104 @@ public class Slo implements ModInitializer {
 		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
 			serverProcess.destroy();
 		}));
+	}
+
+	/*public enum ConnectPhase {
+		INIT,
+		LOADING,
+		READY,
+		ERROR
+	}
+
+	public record ConnectInfo(ConnectPhase phase, int progress) {
+
+		public ConnectInfo(ConnectPhase phase) {
+			this(phase, 0);
+		}
+	}*/
+
+	public static void connect(Minecraft minecraft, ServerLevelSummary serverLevelSummary) throws IOException, ExecutionException, InterruptedException {
+		//BlockingQueue<ConnectInfo> queue = new LinkedBlockingQueue<>();
+		var builder = new ProcessBuilder(Slo.JAVA_PATH, "-jar", serverLevelSummary.extendedDirectory.slo$jarPath());
+		builder.directory(serverLevelSummary.directory.path().toFile());
+		Slo.serverProcess = builder.start();
+		minecraft.execute(() -> {
+			minecraft.setScreen(new GenericMessageScreen(Component.literal("Starting server...")));
+		});
+		new Thread(() -> {
+			var progressListener = LoggerChunkProgressListener.create(4);
+			try (var reader = new BufferedReader(new InputStreamReader(Slo.serverProcess.getInputStream()))) {
+				String line;
+				while ((line = reader.readLine()) != null) {
+					Slo.LOGGER.info(line);
+					if (line.contains("Starting minecraft server version")) {
+						//queue.put(new ConnectInfo(ConnectPhase.INIT));
+						minecraft.execute(() -> {
+							minecraft.setScreen(new GenericMessageScreen(Component.literal("Initializing server...")));
+						});
+					}
+					if (line.contains("Preparing level")) {
+						minecraft.execute(() -> {
+							minecraft.setScreen(new GenericMessageScreen(Component.literal("Loading worlds...")));
+						});
+					}
+					//var prepMatcher = PREPARING_SPAWN.matcher(line);
+					//if (prepMatcher.find())
+					/*if (line.contains("Preparing spawn area: ")) {
+						//queue.put(new ConnectInfo(ConnectPhase.LOADING));
+						minecraft.execute(() -> {
+							minecraft.setScreen(new GenericMessageScreen(Component.literal("loading")));
+						});
+					}*/
+					if (line.contains("For help, type \"help\"")) {
+						//queue.put(new ConnectInfo(ConnectPhase.READY));
+						minecraft.execute(() -> {
+							var serverData = new ServerData(serverLevelSummary.extendedDirectory.slo$levelName(), "localhost", ServerData.Type.OTHER);
+							ConnectScreen.startConnecting(minecraft.screen, minecraft, ServerAddress.parseString("localhost"), serverData, false, null);
+						});
+					}
+				}
+			} catch (IOException e) {
+				// TODO: probably exit
+				Slo.LOGGER.error("Failed to read line", e);
+			}/* catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }*/
+        }).start();
+		Slo.serverProcess.onExit().thenAccept(process -> {
+            /*try {
+                queue.put(new ConnectInfo(ConnectPhase.ERROR));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }*/
+			minecraft.execute(() ->
+				minecraft.forceSetScreen(new DisconnectedScreen(minecraft.screen, CommonComponents.CONNECT_FAILED, Component.literal("exit code: " + process.exitValue())))
+			);
+			Slo.serverProcess = null;
+		});
+		/*new Thread(() -> {
+			while (true) {
+				var info = queue.poll(100, TimeUnit.MILLISECONDS);
+				if (info == null) {
+					continue;
+				}
+				switch (info.phase()) {
+					case INIT -> {
+						minecraft.setScreen(new GenericMessageScreen(Component.literal("initializing")));
+					}
+					case LOADING -> {
+						minecraft.setScreen(new GenericMessageScreen(Component.literal("loading")));
+					}
+					case READY -> {
+
+						return;
+					}
+				}
+			}
+		});*/
+
+		//
+		//loaded.get();
+
 	}
 }
--- src/main/java/com/acikek/slo/mixin/WorldListEntryMixin.java
+++ src/main/java/com/acikek/slo/mixin/WorldListEntryMixin.java
@@ -3,10 +3,7 @@ package com.acikek.slo.mixin;
 import com.acikek.slo.util.ServerLevelSummary;
 import com.acikek.slo.Slo;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screens.ConnectScreen;
 import net.minecraft.client.gui.screens.worldselection.WorldSelectionList;
-import net.minecraft.client.multiplayer.ServerData;
-import net.minecraft.client.multiplayer.resolver.ServerAddress;
 import net.minecraft.world.level.storage.LevelSummary;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,10 +12,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 @Mixin(WorldSelectionList.WorldListEntry.class)
@@ -26,32 +20,13 @@ public class WorldListEntryMixin {
 
     @Shadow @Final LevelSummary summary;
 
+    @Shadow @Final private Minecraft minecraft;
+
     @Inject(method = "joinWorld", at = @At("HEAD"), cancellable = true)
     private void a(CallbackInfo ci) throws IOException, ExecutionException, InterruptedException {
-        if (!summary.primaryActionActive() || !(summary instanceof ServerLevelSummary serverLevelSummary)) {
-            return;
+        if (summary.primaryActionActive() && summary instanceof ServerLevelSummary serverLevelSummary) {
+            ci.cancel();
+            Slo.connect(minecraft, serverLevelSummary);
         }
-        ci.cancel();
-        CompletableFuture<Void> loaded = new CompletableFuture<>();
-        var builder = new ProcessBuilder(Slo.JAVA_PATH, "-jar", serverLevelSummary.extendedDirectory.slo$jarPath());
-        builder.directory(serverLevelSummary.directory.path().toFile());
-        Slo.serverProcess = builder.start();
-        var readThread = new Thread(() -> {
-            try (var reader = new BufferedReader(new InputStreamReader(Slo.serverProcess.getInputStream()))) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    Slo.LOGGER.info(line);
-                    if (line.contains("For help, type \"help\"")) {
-                        loaded.complete(null);
-                    }
-                }
-            } catch (IOException e) {
-                Slo.LOGGER.error("bruh", e);
-            }
-        });
-        readThread.start();
-        loaded.get();
-        var serverData = new ServerData(serverLevelSummary.extendedDirectory.slo$levelName(), "localhost", ServerData.Type.OTHER);
-        ConnectScreen.startConnecting(Minecraft.getInstance().screen, Minecraft.getInstance(), ServerAddress.parseString("localhost"), serverData, false, null);
     }
 }
