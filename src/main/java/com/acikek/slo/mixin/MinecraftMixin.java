db589aad32d5f13797bf42cda6c92f12c129c0a3
Skye Prince <spiralixp@gmail.com>
2025-05-15 15:24:13 -0700

    exit screen on disconnect

--- src/main/java/com/acikek/slo/mixin/MinecraftMixin.java
+++ src/main/java/com/acikek/slo/mixin/MinecraftMixin.java
@@ -1,8 +1,13 @@
 package com.acikek.slo.mixin;
 
 import com.acikek.slo.Slo;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.TitleScreen;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -10,18 +15,27 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.concurrent.ExecutionException;
 
 @Mixin(Minecraft.class)
-public class MinecraftMixin {
+public abstract class MinecraftMixin {
 
-    @Inject(method = "disconnect(Lnet/minecraft/client/gui/screens/Screen;Z)V", at = @At("HEAD"))
-    private void slo$stopServerProcess(CallbackInfo ci) throws IOException {
-        if (Slo.serverProcess != null) {
-            var stdin = Slo.serverProcess.getOutputStream();
-            var writer = new BufferedWriter(new OutputStreamWriter(stdin));
-            writer.write("stop");
-            writer.flush();
-            writer.close();
+    @Shadow public abstract void setScreen(@Nullable Screen screen);
+
+    @Inject(method = "disconnect(Lnet/minecraft/client/gui/screens/Screen;Z)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;updateScreenAndTick(Lnet/minecraft/client/gui/screens/Screen;)V"), cancellable = true)
+    private void slo$stopServerProcess(CallbackInfo ci, @Local(argsOnly = true) Screen screen) throws IOException, ExecutionException, InterruptedException {
+        if (Slo.serverProcess == null) {
+            return;
         }
+        var stdin = Slo.serverProcess.getOutputStream();
+        var writer = new BufferedWriter(new OutputStreamWriter(stdin));
+        writer.write("stop");
+        writer.flush();
+        writer.close();
+        Slo.serverProcess.onExit().thenRunAsync(() -> {
+            ((Minecraft) (Object) this).execute(() -> {
+                setScreen(new TitleScreen());
+            });
+        });
     }
 }
--- src/main/java/com/acikek/slo/mixin/PauseScreenMixin.java
+++ src/main/java/com/acikek/slo/mixin/PauseScreenMixin.java
@@ -2,9 +2,14 @@ package com.acikek.slo.mixin;
 
 import com.acikek.slo.Slo;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.GenericMessageScreen;
 import net.minecraft.client.gui.screens.PauseScreen;
+import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PauseScreen.class)
 public class PauseScreenMixin {
@@ -14,8 +19,11 @@ public class PauseScreenMixin {
         return Slo.serverProcess != null;
     }
 
-    @ModifyExpressionValue(method = "onDisconnect", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;isLocalServer()Z"))
-    private boolean slo$disconnectToTitle(boolean original) {
-        return Slo.serverProcess != null;
+    @Inject(method = "onDisconnect", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;disconnect()V"), cancellable = true)
+    private void slo$disconnectToTitle(CallbackInfo ci) {
+        if (Slo.serverProcess != null) {
+            ci.cancel();
+            Minecraft.getInstance().disconnect(new GenericMessageScreen(Component.translatable("menu.savingLevel")));
+        }
     }
 }
