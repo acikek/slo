8215b0844dba2a7397f1f48f2a185b11e84b4791
Skye Prince <spiralixp@gmail.com>
2025-05-16 18:18:58 -0700

    move connect thread code to its own method

--- src/main/java/com/acikek/slo/Slo.java
+++ src/main/java/com/acikek/slo/Slo.java
@@ -42,32 +42,7 @@ public class Slo implements ModInitializer {
 		minecraft.execute(() -> {
 			minecraft.setScreen(new GenericMessageScreen(Component.literal("Starting server...")));
 		});
-		new Thread(() -> {
-			try (var reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-				String line;
-				boolean preparing = false;
-				var logger = LoggerFactory.getLogger(MOD_ID + "/" + serverLevelSummary.extendedDirectory.slo$levelName());
-				while ((line = reader.readLine()) != null) {
-					logger.info(line);
-					if (line.contains("Starting minecraft server version")) {
-						minecraft.execute(() -> minecraft.setScreen(new GenericMessageScreen(Component.literal("Initializing server..."))));
-					}
-					else if (!preparing && line.contains("Preparing level")) {
-						minecraft.execute(() -> minecraft.setScreen(new GenericMessageScreen(Component.literal("Loading worlds..."))));
-						preparing = true;
-					}
-					else if (line.contains("For help, type \"help\"")) {
-						minecraft.execute(() -> {
-							var serverData = new ServerData(serverLevelSummary.extendedDirectory.slo$levelName(), "localhost", ServerData.Type.OTHER);
-							ConnectScreen.startConnecting(parent, minecraft, ServerAddress.parseString("localhost"), serverData, false, null);
-							Slo.serverProcess = process;
-						});
-					}
-				}
-			} catch (IOException e) {
-				Slo.LOGGER.error("Failed to read server input", e);
-			}
-        }).start();
+		new Thread(() -> tryReadServerInput(minecraft, parent, serverLevelSummary, process)).start();
 		process.onExit().thenAccept(exited ->
 			minecraft.execute(() -> {
 				if (Slo.serverProcess == null) {
@@ -80,5 +55,32 @@ public class Slo implements ModInitializer {
 				Slo.serverProcess = null;
 			})
 		);
+	}
+
+	public static void tryReadServerInput(Minecraft minecraft, Screen parent, ServerLevelSummary serverLevelSummary, Process process) {
+		try (var reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+			String line;
+			boolean preparing = false;
+			var logger = LoggerFactory.getLogger(MOD_ID + "/" + serverLevelSummary.extendedDirectory.slo$levelName());
+			while ((line = reader.readLine()) != null) {
+				logger.info(line);
+				if (line.contains("Starting minecraft server version")) {
+					minecraft.execute(() -> minecraft.setScreen(new GenericMessageScreen(Component.literal("Initializing server..."))));
+				}
+				else if (!preparing && line.contains("Preparing level")) {
+					minecraft.execute(() -> minecraft.setScreen(new GenericMessageScreen(Component.literal("Loading worlds..."))));
+					preparing = true;
+				}
+				else if (line.contains("For help, type \"help\"")) {
+					minecraft.execute(() -> {
+						var serverData = new ServerData(serverLevelSummary.extendedDirectory.slo$levelName(), "localhost", ServerData.Type.OTHER);
+						ConnectScreen.startConnecting(parent, minecraft, ServerAddress.parseString("localhost"), serverData, false, null);
+						Slo.serverProcess = process;
+					});
+				}
+			}
+		} catch (IOException e) {
+			Slo.LOGGER.error("Failed to read server input", e);
+		}
 	}
 }
